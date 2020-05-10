84dcd1d8586063c3343686bf493c00b8ce10aad7
Luke Bennett <hello@lukebennett.com.au>
2020-05-10 19:32:22 +1000

    Add search bar and animate

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useTransition, animated } from 'react-spring';
 import { Link } from 'gatsby';
 
-import { useGraphQL, useCartCount } from '../hooks';
+import { useGraphQL, useCartCount, useStoreContext } from '../hooks';
 import { Wave } from './wave';
+import { SearchBar } from './search-bar';
+import { AddToCartPreview } from './add-to-cart-preview';
 
 const Header = ({ setMenuOpen }) => {
   const count = useCartCount();
@@ -17,10 +20,18 @@ const Header = ({ setMenuOpen }) => {
     setMenuOpen((prevState) => !prevState);
   }
 
+  const { isAddedToCart } = useStoreContext();
+
+  const transitions = useTransition(count >= 1, null, {
+    from: { opacity: 0, transform: 'translate3d(0, 1rem, 0)' },
+    enter: { opacity: 1, transform: 'translate3d(0, 0rem, 0)' },
+    leave: { opacity: 0, transform: 'translate3d(0, -1rem, 0)' },
+  });
+
   return (
-    <header className="overflow-hidden">
-      <div className="bg-pastel-teal">
-        <div className="relative z-10 flex flex-shrink-0 h-16 bg-white shadow">
+    <header className="relative">
+      <div className="relative bg-teal-300">
+        <div className="relative z-10 flex flex-shrink-0 w-full h-16 mx-auto bg-white shadow max-w-7xl md:bg-transparent md:shadow-none">
           <button
             type="button"
             onClick={toggleMenu}
@@ -41,41 +52,13 @@ const Header = ({ setMenuOpen }) => {
               />
             </svg>
           </button>
-          <div className="flex justify-between flex-1 px-4">
-            <div className="flex flex-1">
-              <div className="flex w-full md:ml-0">
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label htmlFor="search_field" className="sr-only">
-                  Search
-                </label>
-                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                  <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                    <svg
-                      className="w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                      />
-                    </svg>
-                  </div>
-                  <input
-                    id="search_field"
-                    className="block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 rounded-md focus:outline-none focus:placeholder-gray-400 sm:text-sm"
-                    placeholder="Search"
-                    type="search"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="flex items-center ml-4 md:ml-6">
-              <button
-                type="button"
-                aria-label="Notifications"
-                className="p-1 text-gray-400 rounded-full hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:shadow-outline focus:text-gray-500"
+          <div className="flex justify-between flex-1 px-4 md:mt-4">
+            <SearchBar />
+            <div className="relative flex items-center ml-4 md:ml-6">
+              <Link
+                to="/cart/"
+                aria-label="Shopping cart"
+                className="relative p-3 text-gray-500 bg-white rounded-full md:shadow hover:bg-gray-100 hover:text-gray-600 focus:outline-none focus:shadow-outline focus:text-gray-600"
               >
                 <svg
                   fill="none"
@@ -88,29 +71,36 @@ const Header = ({ setMenuOpen }) => {
                 >
                   <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
-              </button>
-
-              <div className="relative ml-3">
-                <div>
-                  <Link
-                    to="/cart/"
-                    aria-label="User menu"
-                    aria-haspopup="true"
-                    className="flex items-center max-w-xs text-sm rounded-full focus:outline-none focus:shadow-outline"
-                    id="user-menu"
-                  >
-                    {count}
-                  </Link>
-                </div>
-              </div>
+                {transitions.map(
+                  ({ item, key, props: style }) =>
+                    item && (
+                      <animated.div
+                        key={key}
+                        style={style}
+                        className="absolute top-0 right-0 pointer-events-none"
+                      >
+                        <span
+                          className={`${
+                            count >= 10 ? 'px-2' : 'w-5'
+                          } flex items-center justify-center h-5 max-w-xs pt-1 font-mono text-xs font-bold text-white transform translate-x-1/2 bg-gray-900 rounded-full shadow-lg pointer-events-auto focus:outline-none focus:shadow-outline`}
+                        >
+                          {count}
+                        </span>
+                      </animated.div>
+                    )
+                )}
+              </Link>
+              {isAddedToCart && <AddToCartPreview />}
             </div>
           </div>
         </div>
 
-        <div className="px-4 pt-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="px-4 pt-12 pb-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="text-center text-gray-900">
             <h1 className="font-mono text-2xl leading-none sm:text-6xl">
-              <Link to="/">{title}</Link>
+              <Link to="/" className=" focus:outline-none focus:underline">
+                {title}
+              </Link>
             </h1>
             <p className="mt-1 leading-tight lowercase sm:text-xl">
               {description}
@@ -118,7 +108,9 @@ const Header = ({ setMenuOpen }) => {
           </div>
         </div>
       </div>
-      <Wave className="w-full -mb-1 text-pastel-teal" />
+      <div className="overflow-hidden">
+        <Wave className="-mx-1 -mt-px text-teal-300" />
+      </div>
     </header>
   );
 };
---
+++ src/components/search-bar.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'gatsby';
+
+import { useGraphQL } from '../hooks';
+import { resizeShopifyImage } from '../utils';
+
+export function SearchBar() {
+  const {
+    allShopifyProduct: { nodes: products },
+  } = useGraphQL();
+
+  // Add state for query
+  const [query, setQuery] = useState('');
+
+  // Add state for search results
+  const [searchResults, setSearchResults] = useState([]);
+
+  // When query changes, update the state
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
+  useEffect(() => {
+    // Whenever query is not an empty string, update the productResults
+    if (query !== '') {
+      const results = products.filter((product) =>
+        product.title.toLowerCase().includes(query.toLowerCase())
+      );
+      setSearchResults(results);
+    }
+    // If the user starts typing, and then deletes their query, set it back to an empty array
+    else setSearchResults([]);
+  }, [products, query]);
+
+  return (
+    <div className="relative flex flex-1">
+      <div className="flex flex-1 pl-6 pr-2 overflow-hidden bg-white md:rounded-full md:shadow">
+        <div className="flex w-full md:ml-0">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="search_field" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+            <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                />
+              </svg>
+            </div>
+            <input
+              id="search_field"
+              className="block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 rounded-lg focus:outline-none focus:placeholder-gray-400 sm:text-sm"
+              placeholder="Search"
+              type="search"
+              onChange={handleChange}
+              value={query}
+            />
+          </div>
+        </div>
+        {searchResults.length > 1 && (
+          <div className="absolute inset-x-0 top-0 z-10 p-4 overflow-hidden text-left bg-white rounded-lg shadow-2xl mt-13 full-bleed md:reset-full-bleed">
+            <div className="shadow-sm">
+              <h3 className="px-4 pb-4 uppercase">Products</h3>
+              <ul className="bg-white border-t">
+                {searchResults.splice(0, 4).map((result) => (
+                  <li
+                    key={result.handle}
+                    className="-mx-4 hover:bg-gray-200 odd:bg-gray-50"
+                  >
+                    <Link
+                      to={`/products/${result.handle}`}
+                      className="flex items-center px-8 py-2"
+                    >
+                      <img
+                        src={
+                          result.images[0]
+                            ? resizeShopifyImage({
+                                url: result.images[0].originalSrc,
+                                width: 44,
+                              })
+                            : ''
+                        }
+                        alt=""
+                        width={44}
+                        height={44}
+                        className="object-contain bg-white h-11 w-11"
+                      />
+                      <span className="ml-2">{result.title}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="px-4 pt-4 border-t">Searching for: "{query}"</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
