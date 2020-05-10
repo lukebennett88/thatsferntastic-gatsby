6b641607cfe13daf8b5c3b3e91e59d1028427bf0
Luke Bennett <hello@lukebennett.com.au>
2020-05-10 19:34:16 +1000

    Use rounder corners

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -15,7 +15,7 @@ export default function Sidebar() {
                 key={navItem.slug}
                 to={navItem.slug}
                 activeClassName="text-gray-900 bg-gray-100 hover:bg-gray-100 focus:bg-gray-200"
-                className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-gray-600 transition duration-150 ease-in-out rounded-md group first:mt-0 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-100"
+                className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-gray-600 transition duration-150 ease-in-out rounded-lg group first:mt-0 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-100"
               >
                 <navItem.icon className="w-6 h-6 mr-3 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-500 group-focus:text-gray-600" />
                 {navItem.title}
