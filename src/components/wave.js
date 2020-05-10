e59216b2c5bb056b4209ef5ccab6a5011cc8ddfc
Luke Bennett <hello@lukebennett.com.au>
2020-05-10 19:33:59 +1000

    Update vector

--- src/components/wave.js
+++ src/components/wave.js
@@ -3,15 +3,8 @@ import React from 'react';
 
 export function Wave({ className }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1440 320"
-      className={className}
-    >
-      <path
-        fill="currentColor"
-        d="M0 96l48 16c48 16 144 48 240 48s192-32 288-53.3C672 85 768 75 864 85.3 960 96 1056 128 1152 128s192-32 240-48l48-16V0H0z"
-      />
+    <svg viewBox="0 0 1440 96" fill="currentColor" className={className}>
+      <path d="M0 32l48 16c48 16 144 48 240 48s192-32 288-53.3C672 21 768 11 864 21.3 960 32 1056 64 1152 64s192-32 240-48l48-16H0v32z" />
     </svg>
   );
 }
