4a7f22854424fac6bdd66ded58bc690bd0c80616
Luke Bennett <hello@lukebennett.com.au>
2020-05-14 23:28:44 +1000

    Create pages for collections

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,42 @@
 const path = require('path');
 
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
-  const result = await graphql(`
+  const products = await graphql(`
     query {
-      allShopifyProduct(sort: { fields: [title] }) {
+      allShopifyProduct(sort: { fields: handle, order: DESC }) {
         nodes {
-          id
           handle
+          id
         }
       }
     }
   `);
 
-  result.data.allShopifyProduct.nodes.forEach((node) => {
+  products.data.allShopifyProduct.nodes.forEach((node) => {
     createPage({
       path: `/products/${node.handle}`,
       component: path.resolve(`./src/templates/product.js`),
+      context: {
+        productId: node.id,
+      },
+    });
+  });
+
+  const collections = await graphql(`
+    query {
+      allShopifyCollection(sort: { fields: handle, order: DESC }) {
+        nodes {
+          handle
+          id
+        }
+      }
+    }
+  `);
+
+  collections.data.allShopifyCollection.nodes.forEach((node) => {
+    createPage({
+      path: `/collections/${node.handle}`,
+      component: path.resolve(`./src/templates/collection.js`),
       context: {
         productId: node.id,
       },
---
+++ src/templates/collection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { Layout, SEO, Tile } from '../components';
+import { useGraphQL } from '../hooks';
+
+export default function CollectionPageTemplate() {
+  const {
+    allShopifyProduct: { nodes: products },
+  } = useGraphQL();
+
+  return (
+    <Layout hasHero>
+      <SEO title="Collection" />
+      <div className="relative grid max-w-lg row-gap-10 col-gap-12 pb-20 mx-auto lg:grid-cols-3 lg:max-w-none">
+        {products.map((product) => (
+          <Tile
+            key={product.handle}
+            slug={product.handle}
+            title={product.title}
+            price={Number(product.priceRange.minVariantPrice.amount)}
+            image={product.images[0]}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
+}
