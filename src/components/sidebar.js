5ef397b74c01a8c2e62663fe596e9c95b3eb9937
Luke Bennett <hello@lukebennett.com.au>
2020-12-29 14:27:00 +1100

    Show product types in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,21 +1,53 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
-import { navigation } from '../data';
+import { useGraphQL } from '../hooks';
 
 function Sidebar() {
+  const { allShopifyProduct } = useGraphQL(graphql`
+    {
+      allShopifyProduct {
+        nodes {
+          productType
+        }
+      }
+    }
+  `);
+
+  const types = [
+    ...new Set(
+      allShopifyProduct.nodes
+        .filter((node) => node.productType !== '')
+        .map((node) => node.productType)
+    ),
+  ];
+
+  const colours = [
+    'bg-pink-200 group-hover:bg-pink-300 group-focus:bg-pink-400',
+    'bg-teal-200 group-hover:bg-teal-300 group-focus:bg-teal-400',
+    'bg-yellow-200 group-hover:bg-yellow-300 group-focus:bg-yellow-400',
+    'bg-cyan-200 group-hover:bg-cyan-300 group-focus:bg-cyan-400',
+  ];
+
   return (
     <aside className="hidden lg:block">
       <nav className="sticky rounded-lg top-28">
-        {navigation.map((navItem) => (
+        {types.map((type, index) => (
           <Link
-            key={navItem.slug}
-            to={navItem.slug}
+            key={type}
+            to={`/products/?q=${type.split(' ').join('-').toLowerCase()}/`}
             activeClassName="text-gray-900 bg-gray-100 hover:bg-gray-100 focus:bg-gray-200"
             className="flex items-center px-2 py-2 mt-1 text-sm font-medium leading-5 text-gray-600 transition duration-150 ease-in-out rounded-lg group first:mt-0 hover:text-gray-900 hover:bg-gray-50 focus:bg-gray-100"
           >
-            <navItem.icon className="w-6 h-6 mr-3 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-500 group-focus:text-gray-600" />
-            {navItem.title}
+            <span
+              aria-hidden
+              className={`inline-block transition duration-150 ease-in-out rounded-full w-5 h-5 mr-3 group-focus:text-gray-600 ${
+                colours[
+                  index + 1 > colours.length ? index - colours.length : index
+                ]
+              }`}
+            />
+            {type}
           </Link>
         ))}
       </nav>
