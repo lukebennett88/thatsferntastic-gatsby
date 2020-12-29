823a1dd26a4957b93528af8424f028d84abcdf32
Luke Bennett <hello@lukebennett.com.au>
2020-12-29 22:10:17 +1100

    Fix adding variant to cart

--- src/components/option-picker/option-picker.js
+++ src/components/option-picker/option-picker.js
@@ -1,15 +1,15 @@
-import PropTypes from 'prop-types';
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
-export const OptionPicker = ({ name, options, onChange, selected }) => {
+function OptionPicker({ name, options, onChange }) {
   if (options.length <= 1) return null;
+
   return (
     <div>
       <label htmlFor={name.toLowerCase()}>
         <span className="block font-bold">{name}</span>
         <select
           onChange={onChange}
-          value={selected}
           id={name.toLowerCase()}
           className="mt-1 border-gray-300 rounded-md shadow-sm focus:border-transparent focus:ring focus:ring-blue-500 focus:ring-opacity-50"
         >
@@ -22,11 +22,14 @@ export const OptionPicker = ({ name, options, onChange, selected }) => {
       </label>
     </div>
   );
-};
+}
+
+OptionPicker.propTypes = {};
 
 OptionPicker.propTypes = {
-  name: PropTypes.string,
-  onChange: PropTypes.func,
-  options: PropTypes.any,
-  selected: PropTypes.any,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
 };
+
+export { OptionPicker };
--- src/templates/product.js
+++ src/templates/product.js
@@ -1,8 +1,7 @@
-import React, { useState, useMemo } from 'react';
+import * as React from 'react';
 import GatsbyImage from 'gatsby-image';
 import { graphql } from 'gatsby';
 import { useAddItemToCart } from 'gatsby-theme-shopify-manager';
-import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 
 import {
@@ -15,34 +14,23 @@ import {
 import { prepareVariantsWithOptions } from '../utils';
 
 function ProductPage({ data: { shopifyProduct: product } }) {
-  const variants = useMemo(() => prepareVariantsWithOptions(product.variants), [
-    product.variants,
-  ]);
-
   const addItemToCart = useAddItemToCart();
-  const [variant, setVariant] = useState(variants[0]);
-  const [isAlertShown, setIsAlertShown] = useState(false);
-  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const [isAlertShown, setIsAlertShown] = React.useState(false);
+  const [activeImageIndex, setActiveImageIndex] = React.useState(0);
+  const variants = React.useMemo(
+    () => prepareVariantsWithOptions(product.variants),
+    [product.variants]
+  );
+  const [variant, setVariant] = React.useState(() => variants[0]);
 
-  const { register, handleSubmit } = useForm();
-
-  async function handleAddToCart(options) {
+  async function handleAddToCart() {
     try {
-      await addItemToCart(variant.shopifyId, 1, options);
+      await addItemToCart(variant.shopifyId, 1);
       setIsAlertShown(true);
     } catch (e) {
       setIsAlertShown(false);
     }
   }
-
-  const onSubmit = (data) => {
-    const options = Object.keys(data).map((key) => {
-      const opt = { key, value: data[key] };
-      setVariant(opt);
-      return opt;
-    });
-    handleAddToCart(options);
-  };
 
   return (
     <Layout hasSidebar={false}>
@@ -75,7 +63,8 @@ function ProductPage({ data: { shopifyProduct: product } }) {
             <h1 className="hidden text-lg font-bold leading-tight md:block">
               {product.title}
             </h1>
-            {Number(variant.priceV2.amount) > 0 && (
+
+            {Number(variant?.priceV2?.amount) > 0 && (
               <dl className="mt-4">
                 <dt className="sr-only">Price:</dt>
                 <dd className="font-bold">
@@ -90,27 +79,34 @@ function ProductPage({ data: { shopifyProduct: product } }) {
                   key={index}
                   name={option.name}
                   options={option.values}
-                  register={register}
+                  onChange={(e) =>
+                    setVariant(
+                      variants.filter(
+                        (v) =>
+                          v.selectedOptions[0].name === option.name &&
+                          v.selectedOptions[0].value === e.target.value
+                      )[0]
+                    )
+                  }
                 />
               ))}
             </div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <span className="relative inline-flex transition duration-300 ease-in-out transform rounded-full shadow-sm hover:-translate-y-1 hover:shadow-lg">
-                <button
-                  type="submit"
-                  className="inline-flex items-center px-6 py-3 font-mono text-base font-medium text-pink-700 bg-pink-100 border border-transparent rounded-full hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
-                >
-                  <span className="leading-none">Add to Cart</span>
-                </button>
-              </span>
-            </form>
+            <span className="relative transition duration-300 ease-in-out transform rounded-full hover:-translate-y-0.5">
+              <button
+                type="button"
+                onClick={handleAddToCart}
+                className="inline-flex items-center px-6 py-3 font-mono text-base font-medium text-pink-700 bg-pink-100 border border-transparent rounded-full hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 hover:shadow-lg"
+              >
+                <span className="leading-none">Add to Cart</span>
+              </button>
+            </span>
 
             <div
               dangerouslySetInnerHTML={{
                 __html: product.descriptionHtml,
               }}
-              className="grid mt-6 text-base leading-6 prose text-gray-700 gap-y-4"
+              className="mt-6 prose"
             />
           </div>
         </article>
