c1e19de90ac23c7fb92c5ad88ad865c8f254c890
Luke Bennett <hello@lukebennett.com.au>
2020-12-30 16:52:28 +1100

    Redesign cart page

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -10,23 +10,32 @@ import { LineItem } from './line-item';
 
 export function Cart() {
   const lineItems = useCartItems();
-  const cart = useCart();
   const count = useCartCount();
-  const checkout = useCheckoutUrl();
+
   return (
     <div className="relative pb-20">
       <h1 className="heading-1">Cart</h1>
-      <div className="mt-3 sm:mt-4">
-        {count
-          ? lineItems.map((item) => (
-              <React.Fragment key={item.id}>
-                <LineItem key={item.id} item={item} />
-                <hr className="my-4" />
-              </React.Fragment>
-            ))
-          : 'Nothing to see here, your cart is empty!'}
-        <div className="flex">
-          <div className="w-full px-4 py-6 mx-auto bg-white rounded-lg shadow md:max-w-xs md:mr-0">
+      <div className="flex flex-wrap py-12 lg:space-x-16">
+        <div className="relative grid flex-1 pb-20 mx-auto gap-y-10 gap-x-12 sm:grid-cols-2 lg:grid-cols-1 lg:col-span-3">
+          {count
+            ? lineItems.map((item) => <LineItem key={item.id} item={item} />)
+            : 'Nothing to see here, your cart is empty!'}
+        </div>
+        <CartSummary />
+      </div>
+    </div>
+  );
+}
+
+function CartSummary() {
+  const count = useCartCount();
+  const cart = useCart();
+  const checkout = useCheckoutUrl();
+  return (
+    <>
+      <aside className="w-full lg:max-w-xs">
+        <nav className="sticky top-28">
+          <div className="w-full px-4 py-6 bg-white rounded-lg shadow">
             <h3 className="text-lg font-medium leading-6 text-gray-900">
               Cart Summary
             </h3>
@@ -53,8 +62,35 @@ export function Cart() {
               </a>
             </div>
           </div>
+        </nav>
+      </aside>
+      {/* <div className="w-full px-4 py-6 mx-auto bg-white rounded-lg shadow md:max-w-xs md:mr-0">
+      <h3 className="text-lg font-medium leading-6 text-gray-900">
+        Cart Summary
+      </h3>
+      <hr className="my-3" />
+      <dl className="grid gap-y-3">
+        <div className="flex justify-between">
+          <dt>Items in cart:</dt>
+          <dd>{count}</dd>
         </div>
+        <div className="flex justify-between">
+          <dt>Subtotal:</dt>
+          <dd>${Number(cart?.totalPrice || 0).toFixed(2)}</dd>
+        </div>
+      </dl>
+      <div className="mt-6">
+        <a
+          aria-hidden={!count}
+          href={count ? checkout : null}
+          className={`inline-flex items-center justify-center w-full px-6 py-3 font-mono text-base font-medium text-center text-pink-700 bg-pink-100 border border-transparent rounded-full hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 hover:shadow-lg ${
+            !count ? 'cursor-not-allowed opacity-75' : ''
+          }`}
+        >
+          Checkout
+        </a>
       </div>
-    </div>
+    </div> */}
+    </>
   );
 }
--- src/components/cart/line-item.js
+++ src/components/cart/line-item.js
@@ -8,8 +8,6 @@ import {
 import { HiChevronLeft, HiChevronRight, HiTrash } from 'react-icons/hi';
 import PropTypes from 'prop-types';
 
-import { Spinner } from '../spinner';
-
 function LineItem({ item }) {
   const {
     allShopifyProductVariant: { nodes: variants },
@@ -22,7 +20,7 @@ function LineItem({ item }) {
           image {
             localFile {
               childImageSharp {
-                fluid(maxWidth: 120) {
+                fluid(maxWidth: 630) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }
@@ -83,90 +81,92 @@ function LineItem({ item }) {
     setQuantity((prev) => prev + 1);
   }
 
-  React.useEffect(() => {
-    if (quantity <= 0) {
-      removeFromCart(item.variant.id);
-      return;
-    }
-    updateQuantity(item.variant.id, quantity);
-  }, [item.variant.id, quantity, removeFromCart, updateQuantity]);
-
-  const [isLoaded, setIsLoaded] = React.useState(false);
+  // React.useEffect(() => {
+  //   if (quantity <= 0) {
+  //     removeFromCart(item.variant.id);
+  //     return;
+  //   }
+  //   updateQuantity(item.variant.id, quantity);
+  // }, [item.variant.id, quantity, removeFromCart, updateQuantity]);
 
   return (
-    <div className="md:flex md:items-center md:justify-between">
-      <div className="md:flex md:items-center">
-        <div className="flex-shrink-0 rounded-lg shadow md:w-48">
-          <div className="relative aspect-w-1 aspect-h-1">
-            <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-gray-50">
-              <GatsbyImage
-                fluid={getImageFluidForVariant(item.variant.id)}
-                onLoad={() => setIsLoaded(true)}
-              />
-              {!isLoaded && <Spinner />}
-            </div>
+    <div className="flex max-w-sm px-4 py-6 pt-4 bg-white rounded-lg shadow lg:max-w-none lg:items-center lg:justify-between">
+      <div className="flex flex-col w-full lg:flex-row lg:items-center">
+        <div className="relative w-full h-0 lg:h-auto lg:w-48 aspect-w-1 aspect-h-1 lg:aspect-none">
+          <div className="absolute inset-0 flex bg-white">
+            <GatsbyImage
+              fluid={getImageFluidForVariant(item.variant.id)}
+              className="flex-1 rounded-lg"
+            />
           </div>
         </div>
-        <div className="mt-4 md:ml-4">
+        <div className="flex flex-col flex-1 lg:ml-4">
           <Link
             to={`/products/${getHandleForVariant(item.variant.id)}`}
-            className="text-lg font-medium leading-6 text-gray-900 underline transition duration-150 ease-in-out hover:text-gray-600"
+            className="text-lg font-medium transition duration-150 ease-in-out hover:text-gray-600"
           >
             {item.title}
           </Link>
-          <dl className="mt-2 text-base leading-6 text-gray-500">
-            {item.variant.selectedOptions.map(({ name, value }) => (
-              <div key={name}>
-                <dt className="inline font-medium text-gray-500">{name}: </dt>
-                <dd className="inline mt-1 text-gray-900 sm:mt-0 sm:col-span-2">
-                  {value}
-                </dd>
-              </div>
-            ))}
+          <dl className="mt-2 text-base text-gray-500">
+            {item.variant.selectedOptions.map(
+              ({ name, value }) =>
+                name !== 'Title' && (
+                  <div key={name}>
+                    <dt className="inline font-medium text-gray-500">
+                      {name}:{' '}
+                    </dt>
+                    <dd className="inline mt-1 text-gray-900 sm:mt-0 sm:col-span-2">
+                      {value}
+                    </dd>
+                  </div>
+                )
+            )}
           </dl>
-          <div className="mt-2">
-            <div>Quantity</div>
-            <div className="relative z-0 inline-flex mt-1 -space-x-px shadow-sm">
-              <button
-                type="button"
-                onClick={handleDecreaseQuantity}
-                disabled={item.quantity < 1}
-                className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50 focus:z-10"
-              >
-                <span className="sr-only">
-                  {quantity <= 1 ? 'Remove from cart' : 'Decrease quantity'}
+          <div className="flex items-end justify-between w-full pt-2 mt-auto">
+            <div>
+              <div className="font-medium text-gray-500">Quantity:</div>
+              <div className="relative z-0 inline-flex mt-1 -space-x-px shadow-sm">
+                <button
+                  type="button"
+                  onClick={handleDecreaseQuantity}
+                  disabled={item.quantity < 1}
+                  className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50 focus:z-10"
+                >
+                  <span className="sr-only">
+                    {quantity <= 1 ? 'Remove from cart' : 'Decrease quantity'}
+                  </span>
+                  {quantity <= 1 ? (
+                    <HiTrash className="w-5 h-5" aria-hidden />
+                  ) : (
+                    <HiChevronLeft className="w-5 h-5" aria-hidden />
+                  )}
+                </button>
+                <span className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300">
+                  {quantity}
                 </span>
-                {quantity <= 1 ? (
-                  <HiTrash className="w-5 h-5" aria-hidden />
-                ) : (
-                  <HiChevronLeft className="w-5 h-5" aria-hidden />
-                )}
-              </button>
-              <span className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300">
-                {quantity}
-              </span>
+                <button
+                  type="button"
+                  onClick={handleIncreaseQuantity}
+                  className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50 focus:z-10"
+                >
+                  <span className="sr-only">Increase quantity</span>
+                  <HiChevronRight className="w-5 h-5" aria-hidden />
+                </button>
+              </div>
+            </div>
+            <div className="hidden lg:items-baseline lg:flex">
               <button
+                onClick={() => removeFromCart(item.variant.id)}
                 type="button"
-                onClick={handleIncreaseQuantity}
-                className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50 focus:z-10"
+                className="text-gray-800 underline transition duration-150 ease-in-out hover:text-gray-600"
               >
-                <span className="sr-only">Increase quantity</span>
-                <HiChevronRight className="w-5 h-5" aria-hidden />
+                Remove from cart
               </button>
+              <div className="font-mono text-3xl text-pink-500 lg:ml-4">
+                ${Number(item.variant.priceV2.amount * quantity).toFixed(2)}
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="hidden md:items-baseline md:flex">
-        <button
-          onClick={() => removeFromCart(item.variant.id)}
-          type="button"
-          className="text-gray-800 underline transition duration-150 ease-in-out hover:text-gray-600"
-        >
-          Remove from cart
-        </button>
-        <div className="font-mono text-3xl font-bold text-gray-900 md:ml-4">
-          ${Number(item.variant.priceV2.amount * quantity).toFixed(2)}
         </div>
       </div>
     </div>
