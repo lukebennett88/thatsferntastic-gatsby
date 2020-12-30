48163f995089acfd35370fb103c4ab189af5a639
Luke Bennett <hello@lukebennett.com.au>
2020-12-30 17:02:53 +1100

    Remove commented out code

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -28,69 +28,41 @@ export function Cart() {
 }
 
 function CartSummary() {
-  const count = useCartCount();
   const cart = useCart();
+  const count = useCartCount();
   const checkout = useCheckoutUrl();
+
   return (
-    <>
-      <aside className="w-full lg:max-w-xs">
-        <nav className="sticky top-28">
-          <div className="w-full px-4 py-6 bg-white rounded-lg shadow">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Cart Summary
-            </h3>
-            <hr className="my-3" />
-            <dl className="grid gap-y-3">
-              <div className="flex justify-between">
-                <dt>Items in cart:</dt>
-                <dd>{count}</dd>
-              </div>
-              <div className="flex justify-between">
-                <dt>Subtotal:</dt>
-                <dd>${Number(cart?.totalPrice || 0).toFixed(2)}</dd>
-              </div>
-            </dl>
-            <div className="mt-6">
-              <a
-                aria-hidden={!count}
-                href={count ? checkout : null}
-                className={`inline-flex items-center justify-center w-full px-6 py-3 font-mono text-base font-medium text-center text-pink-700 bg-pink-100 border border-transparent rounded-full hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 hover:shadow-lg ${
-                  !count ? 'cursor-not-allowed opacity-75' : ''
-                }`}
-              >
-                Checkout
-              </a>
+    <aside className="w-full lg:max-w-xs">
+      <nav className="sticky top-28">
+        <div className="w-full px-4 py-6 bg-white rounded-lg shadow">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">
+            Cart Summary
+          </h3>
+          <hr className="my-3" />
+          <dl className="grid gap-y-3">
+            <div className="flex justify-between">
+              <dt>Items in cart:</dt>
+              <dd>{count}</dd>
             </div>
+            <div className="flex justify-between">
+              <dt>Subtotal:</dt>
+              <dd>${Number(cart?.totalPrice || 0).toFixed(2)}</dd>
+            </div>
+          </dl>
+          <div className="mt-6">
+            <a
+              aria-hidden={!count}
+              href={count ? checkout : null}
+              className={`inline-flex items-center justify-center w-full px-6 py-3 font-mono text-base font-medium text-center text-pink-700 bg-pink-100 border border-transparent rounded-full hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 hover:shadow-lg ${
+                !count ? 'cursor-not-allowed opacity-75' : ''
+              }`}
+            >
+              Checkout
+            </a>
           </div>
-        </nav>
-      </aside>
-      {/* <div className="w-full px-4 py-6 mx-auto bg-white rounded-lg shadow md:max-w-xs md:mr-0">
-      <h3 className="text-lg font-medium leading-6 text-gray-900">
-        Cart Summary
-      </h3>
-      <hr className="my-3" />
-      <dl className="grid gap-y-3">
-        <div className="flex justify-between">
-          <dt>Items in cart:</dt>
-          <dd>{count}</dd>
         </div>
-        <div className="flex justify-between">
-          <dt>Subtotal:</dt>
-          <dd>${Number(cart?.totalPrice || 0).toFixed(2)}</dd>
-        </div>
-      </dl>
-      <div className="mt-6">
-        <a
-          aria-hidden={!count}
-          href={count ? checkout : null}
-          className={`inline-flex items-center justify-center w-full px-6 py-3 font-mono text-base font-medium text-center text-pink-700 bg-pink-100 border border-transparent rounded-full hover:bg-pink-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 hover:shadow-lg ${
-            !count ? 'cursor-not-allowed opacity-75' : ''
-          }`}
-        >
-          Checkout
-        </a>
-      </div>
-    </div> */}
-    </>
+      </nav>
+    </aside>
   );
 }
