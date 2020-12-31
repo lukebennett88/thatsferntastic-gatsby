3716538b0f78989187a7fc1bd6a8305cafc7b139
Luke Bennett <hello@lukebennett.com.au>
2020-12-31 17:16:24 +1100

    Clean up 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,15 +6,10 @@ export default function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
-      <div className="relative pt-16 pb-20">
-        <div className="text-center">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-            Page Not Found
-          </h1>
-          <p className="max-w-2xl mx-auto mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
-            We couldn't find the page you were looking for.
-          </p>
-        </div>
+
+      <h1 className="heading-1">Page Not Found</h1>
+      <div className="mt-5 prose">
+        <p className="lead">We couldn't find the page you were looking for.</p>
       </div>
     </Layout>
   );
