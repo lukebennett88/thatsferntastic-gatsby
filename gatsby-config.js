addf66bbf55e744371ec30a746c2d17c6d665fd4
Luke Bennett <hello@lukebennett.com.au>
2020-12-31 19:06:46 +1100

    Update GA code

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          'G-0VBDVLMS88', // Google Analytics / GA
+          'UA-186320316-1', // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
