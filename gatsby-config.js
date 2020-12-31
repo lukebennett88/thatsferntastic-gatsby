1080830b564a22709e7cfa41b46251985ddbcc1b
Luke Bennett <hello@lukebennett.com.au>
2021-01-01 10:54:27 +1100

    Downgrade Gatsby to improve dev perf

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,16 +14,16 @@ module.exports = {
   siteMetadata: {
     siteUrl,
   },
-  flags: {
-    DEV_SSR: true,
-    FAST_DEV: true,
-    FAST_REFRESH: true,
-    LAZY_IMAGES: true,
-    PARALLEL_SOURCING: false,
-    PRESERVE_FILE_DOWNLOAD_CACHE: true,
-    PRESERVE_WEBPACK_CACHE: true,
-    QUERY_ON_DEMAND: true,
-  },
+  // flags: {
+  //   DEV_SSR: true,
+  //   FAST_DEV: true,
+  //   FAST_REFRESH: true,
+  //   LAZY_IMAGES: true,
+  //   PARALLEL_SOURCING: false,
+  //   PRESERVE_FILE_DOWNLOAD_CACHE: true,
+  //   PRESERVE_WEBPACK_CACHE: true,
+  //   QUERY_ON_DEMAND: true,
+  // },
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-netlify',
