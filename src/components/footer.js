692e5c9cc7b1b08f3fa85d5afcf98ec6a30d6c4b
Luke Bennett <hello@lukebennett.com.au>
2021-01-02 09:49:13 +1100

    Redesign footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,38 +1,128 @@
 import * as React from 'react';
 import { OutboundLink } from 'gatsby-plugin-google-gtag';
-import { FiFacebook, FiInstagram, FiLinkedin, FiTwitter } from 'react-icons/fi';
+import { Link } from 'gatsby';
+import { FacebookIcon, InstagramIcon, TwitterIcon } from './icons';
 
 import { useGraphQL } from '../hooks';
 
 const SOCIAL_ICON = {
-  Facebook: FiFacebook,
-  Instagram: FiInstagram,
-  LinkedIn: FiLinkedin,
-  Twitter: FiTwitter,
+  Facebook: FacebookIcon,
+  Instagram: InstagramIcon,
+  Twitter: TwitterIcon,
 };
 
 function Footer() {
-  const { sanitySiteSettings } = useGraphQL();
+  const {
+    sanitySiteSettings,
+    allSanityPage,
+    allShopifyCollection,
+  } = useGraphQL();
   return (
-    <footer className="bg-white">
-      <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center space-x-6 md:order-2">
-          {sanitySiteSettings.socialLinks.map((socialLink) => {
-            const Icon = SOCIAL_ICON[socialLink.socialNetwork];
-            return (
-              <OutboundLink
-                key={socialLink._key}
-                href={socialLink.link}
-                className="text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">{socialLink.socialNetwork}</span>
-                <Icon className="w-6 h-6" />
-              </OutboundLink>
-            );
-          })}
+    <footer className="bg-white" aria-labelledby="footerHeading">
+      <h2 id="footerHeading" className="sr-only">
+        Footer
+      </h2>
+      <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div className="space-y-8 xl:col-span-1">
+            <h2 className="font-mono text-xl text-gray-500">
+              {sanitySiteSettings.title}
+            </h2>
+            <p className="text-base text-gray-500">
+              {sanitySiteSettings.description}
+            </p>
+            <div className="flex space-x-6">
+              {sanitySiteSettings.socialLinks.map(
+                ({ _key, link, socialNetwork }) => {
+                  const Icon = SOCIAL_ICON[socialNetwork];
+                  return (
+                    <OutboundLink
+                      key={_key}
+                      href={link}
+                      className="text-gray-400 hover:text-gray-500"
+                    >
+                      <span className="sr-only">{socialNetwork}</span>
+                      <Icon className="w-6 h-6" />
+                    </OutboundLink>
+                  );
+                }
+              )}
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
+                  Links
+                </h3>
+                <ul className="mt-4 space-y-4">
+                  <li>
+                    <Link
+                      to="/posts/"
+                      className="text-base text-gray-500 hover:text-gray-900"
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <OutboundLink
+                      href="https://www.etsy.com/au/shop/ThatsFerntastic"
+                      className="text-base text-gray-500 hover:text-gray-900"
+                    >
+                      Etsy
+                    </OutboundLink>
+                  </li>
+                  <li>
+                    <OutboundLink
+                      href="https://www.redbubble.com/people/FerntasticArt/shop"
+                      className="text-base text-gray-500 hover:text-gray-900"
+                    >
+                      Redbubble
+                    </OutboundLink>
+                  </li>
+                </ul>
+              </div>
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
+                  Collections
+                </h3>
+                <ul className="mt-4 space-y-4">
+                  {allShopifyCollection.nodes.map(({ id, handle, title }) => (
+                    <li key={id}>
+                      <Link
+                        to={`/collections/${handle}/`}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div className="mt-12 md:mt-0">
+                <h3 className="text-sm font-semibold tracking-wider text-gray-400 uppercase">
+                  Legal
+                </h3>
+                <ul className="mt-4 space-y-4">
+                  {allSanityPage.nodes.map(({ _id, slug, title }) => (
+                    <li key={_id}>
+                      <Link
+                        to={`/${slug.current}/`}
+                        className="text-base text-gray-500 hover:text-gray-900"
+                      >
+                        {title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-base leading-6 text-center text-gray-400">
+        <div className="pt-8 mt-12 border-t border-gray-200">
+          <p className="text-base text-gray-400 xl:text-center">
             &copy; {new Date().getFullYear()} {sanitySiteSettings.title}. All
             rights reserved.
           </p>
