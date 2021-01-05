8a346baa40dfda7deee2f68594e7fc5ab34ca5b2
Luke Bennett <hello@lukebennett.com.au>
2021-01-05 22:44:55 +1100

    Add flex-shrink-0 to circle

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -128,7 +128,7 @@ function NavLink({ to, label, active, Icon }) {
       ) : (
         <span
           aria-hidden
-          className="inline-block w-5 h-5 mr-3 transition duration-150 ease-in-out bg-indigo-200 rounded-full group-focus:text-indigo-600 group-hover:bg-indigo-300 group-focus:bg-indigo-400"
+          className="flex-shrink-0 inline-block w-5 h-5 mr-3 transition duration-150 ease-in-out bg-indigo-200 rounded-full group-focus:text-indigo-600 group-hover:bg-indigo-300 group-focus:bg-indigo-400"
         />
       )}
       {label}
