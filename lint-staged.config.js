b2f172c9bb506204de877d511d72b41cfb41af3f
Luke Bennett <hello@lukebennett.com.au>
2021-03-28 16:55:45 +1100

    Update lint-staged settings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,12 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 module.exports = {
-  '*.{md,mdx,json,yaml,js,jsx}': [
-    `prettier "**/*.{md,mdx,json,yaml,js,jsx}" --write`,
+  '*./src/**/*.{md,mdx,json,yaml,js,jsx}': [
+    'prettier "**/*.{md,mdx,json,yaml,js,jsx}" --write',
+  ],
+  '*./studio/*.{md,mdx,json,yaml,js,jsx}': [
+    'prettier "**/*.{md,mdx,json,yaml,js,jsx}" --write',
+  ],
+  '*./studio/schemas/**/*.{md,mdx,json,yaml,js,jsx}': [
+    'prettier "**/*.{md,mdx,json,yaml,js,jsx}" --write',
   ],
 };
