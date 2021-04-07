29db806ef2b6df3771723d0e7a18ac6ef36a1622
Luke Bennett <hello@lukebennett.com.au>
2021-04-07 18:09:51 +1000

    Add teaser page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,35 +1,13 @@
-import React, { useState } from 'react';
-
-import { Footer } from './footer';
-import { Header } from './header';
-import { MobileMenu } from './mobile-menu';
-import { SEO } from './seo';
-import { Sidebar } from './sidebar';
+import React from 'react';
 
 type LayoutProps = {
   children: React.ReactNode;
-  hasSidebar?: boolean;
 };
 
-function Layout({
-  children,
-  hasSidebar = true,
-}: LayoutProps): React.ReactElement {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+function Layout({ children }: LayoutProps): React.ReactElement {
   return (
     <>
-      <SEO />
-      <div className="flex flex-col min-h-screen bg-gray-50">
-        <Header setMenuOpen={setMenuOpen} />
-        <MobileMenu isMenuOpen={isMenuOpen} setMenuOpen={setMenuOpen} />
-        <div className="flex-1 w-full max-w-2xl px-6 mx-auto lg:max-w-screen-xl">
-          <div className="flex flex-wrap py-12 lg:space-x-16">
-            {hasSidebar && <Sidebar />}
-            <main className="flex-1">{children}</main>
-          </div>
-        </div>
-        <Footer />
-      </div>
+      <main className="flex-1">{children}</main>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,23 @@
-import { useLocation } from '@reach/router';
-import queryString from 'query-string';
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 
-import { InstagramWidget, Layout, ProductTile, SEO } from '../components';
-import { useAllShopifyProducts } from '../hooks/use-all-shopify-products';
-import { useSanitySiteSettings } from '../hooks/use-sanity-site-settings';
+import { Layout, SEO } from '../components';
 
 function IndexPage(): React.ReactElement {
   return (
     <Layout>
       <SEO title="Handmade Pencil Cases, Pouches, Stationery, Accessories and More" />
-      <LatestProducts />
-      <InstagramWidget />
-    </Layout>
-  );
-}
-
-function LatestProducts(): React.ReactElement {
-  const allShopifyProduct = useAllShopifyProducts();
-  const sanitySiteSettings = useSanitySiteSettings();
-
-  const { search } = useLocation();
-
-  const products = search
-    ? allShopifyProduct.nodes.filter(
-        (product) => product.productType === queryString.parse(search).q
-      )
-    : allShopifyProduct.nodes;
-
-  return (
-    <article>
-      <h1 className="sr-only">{sanitySiteSettings.title}</h1>
-      <h2 className="text-center heading-1 sm:text-left">
-        {queryString.parse(search).q || 'Latest Products'}
-      </h2>
-      <div className="relative grid pb-20 mx-auto mt-6 gap-y-10 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
-        {products.slice(0, 18).map((product) => (
-          <ProductTile key={product.id} product={product} />
-        ))}
+      <div className="fixed inset-0">
+        <div className="flex h-full">
+          <StaticImage
+            src="../images/thats-ferntastic-launch-teaser.jpg"
+            alt="April 8th – 8pm, Website Launch"
+            objectFit="contain"
+            className="flex-1"
+          />
+        </div>
       </div>
-    </article>
+    </Layout>
   );
 }
 
