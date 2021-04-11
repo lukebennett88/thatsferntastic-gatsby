c1fffcdd0bb05ec50521a08f850412cceb6f9c4c
Luke Bennett <hello@lukebennett.com.au>
2021-04-11 18:31:35 +1000

    Update announcement-banner.tsx

--- src/components/announcement-banner.tsx
+++ src/components/announcement-banner.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 function AnnouncementBanner(): React.ReactElement {
   return (
     <header className="px-4 py-1 font-semibold text-center text-teal-900 sm:px-6 lg:px-8 bg-teal-50">
-      Flat Rate Shipping! $6 to Australia, $20 Worldwide
+      Flat Rate Shipping! $6 to Australia, $16 Worldwide
     </header>
   );
 }
