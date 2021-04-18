3f19aa678cda2cb909693ec52ab20d69112343cb
Luke Bennett <hello@lukebennett.com.au>
2021-04-18 15:45:04 +1000

    Update icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,8 @@
-import { MenuAlt2Icon } from '@heroicons/react/outline';
+import {
+  HomeIcon,
+  MenuAlt2Icon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Link } from 'gatsby';
 import * as React from 'react';
@@ -35,41 +39,21 @@ function Header(): React.ReactElement {
           <div className="flex justify-between flex-1 px-4 space-x-4 md:space-x-6 md:py-4">
             <div className="relative hidden md:flex md:items-center">
               <Link
-                aria-label="Home page"
                 to="/"
                 className="relative p-3 text-gray-500 bg-white rounded-full md:shadow hover:bg-gray-100 hover:text-gray-600 focus:text-gray-600"
               >
-                <svg
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
+                <span className="sr-only">Home page</span>
+                <HomeIcon aria-hidden className="w-6 h-6" />
               </Link>
             </div>
             <SearchBar />
             <div className="relative flex items-center">
               <Link
                 to="/cart/"
-                aria-label="Shopping cart"
                 className="relative p-3 text-gray-500 bg-white rounded-full md:shadow hover:bg-gray-100 hover:text-gray-600 focus:text-gray-600"
               >
-                <svg
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  className="w-6 h-6"
-                >
-                  <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
+                <span className="sr-only">Shopping cart</span>
+                <ShoppingCartIcon aria-hidden className="w-6 h-6" />
                 <AnimatePresence>
                   {count > 0 ? (
                     <motion.div
--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,3 +1,4 @@
+import { SearchIcon } from '@heroicons/react/outline';
 import {
   Combobox,
   ComboboxInput,
@@ -51,13 +52,7 @@ function SearchBar(): React.ReactElement {
         <div className="flex w-full md:ml-0">
           <div className="relative w-full text-gray-400 focus-within:text-gray-600">
             <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                />
-              </svg>
+              <SearchIcon className="w-5 h-5" aria-hidden />
             </div>
             <ComboboxInput
               ref={inputRef}
