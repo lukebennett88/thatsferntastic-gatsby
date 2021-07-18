aa5f26a556a6f932703b732a52ae149d4e8e10c7
Luke Bennett <luke.bennett@brighte.com.au>
2021-07-18 15:29:59 +1000

    Add StoreClosedMessage to gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,9 @@ import './src/css/tailwind.css';
 
 import * as React from 'react';
 
+import { StoreClosedMessage } from './src/components/store-closed-message.tsx';
 import { ShopifyProvider } from './src/context/shopify.tsx';
+
 
 export function wrapRootElement({ element }) {
   return (
@@ -13,6 +15,7 @@ export function wrapRootElement({ element }) {
       accessToken={process.env.GATSBY_SHOPIFY_ACCESS_TOKEN}
     >
       {element}
+      <StoreClosedMessage />
     </ShopifyProvider>
   );
 }
