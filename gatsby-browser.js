95a4fd7494c8e9ab319e42177e1e137502cad033
Luke Bennett <luke.bennett@brighte.com.au>
2021-09-13 12:25:27 +1000

    Remove StoreClosedMessage

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,6 @@ import './src/css/tailwind.css';
 
 import * as React from 'react';
 
-import { StoreClosedMessage } from './src/components/store-closed-message.tsx';
 import { ShopifyProvider } from './src/context/shopify.tsx';
 
 
@@ -15,7 +14,6 @@ export function wrapRootElement({ element }) {
       accessToken={process.env.GATSBY_SHOPIFY_ACCESS_TOKEN}
     >
       {element}
-      <StoreClosedMessage />
     </ShopifyProvider>
   );
 }
