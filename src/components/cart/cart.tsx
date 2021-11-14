d3b12a05a21a301765178b1686ba6be215eee9ee
Luke Bennett <luke.bennett@brighte.com.au>
2021-11-14 16:14:35 +1100

    Clean up code

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -6,6 +6,7 @@ import { useCart } from '../../hooks/use-cart';
 import { useCartCount } from '../../hooks/use-cart-count';
 import { LineItemsType, useCartItems } from '../../hooks/use-cart-items';
 import { useCheckoutUrl } from '../../hooks/use-checkout-url';
+import { classNames } from '../../utils/classnames';
 import { formatMoney } from '../../utils/format-money';
 import { LineItem } from './line-item';
 
@@ -177,11 +178,11 @@ function Terms({ showDialog, setShowDialog }: TermsProps): React.ReactElement {
 
                   <a
                     href={isChecked ? (checkout as string) : undefined}
-                    className={`button ${
-                      isChecked
-                        ? ''
-                        : '!text-gray-700 !bg-gray-100 cursor-not-allowed opacity-75'
-                    }`}
+                    className={classNames(
+                      'button',
+                      !isChecked &&
+                        '!text-gray-700 !bg-gray-100 cursor-not-allowed opacity-75'
+                    )}
                   >
                     Checkout
                   </a>
@@ -191,79 +192,6 @@ function Terms({ showDialog, setShowDialog }: TermsProps): React.ReactElement {
           </div>
         </Dialog>
       </Transition.Root>
-      {/* <DialogOverlay
-      ref={overlayRef}
-      isOpen={showDialog}
-      onDismiss={close}
-      className="fixed inset-0 z-10 overflow-y-auto bg-black bg-opacity-25"
-    >
-      <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
-        <span
-          className="hidden sm:inline-block sm:align-middle sm:h-screen"
-          aria-hidden          aria-label="Terms and Conditions"
-          ref={contentRef}
-          className="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
-          aria-modal
-        >
-          <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
-            <button
-              type="button"
-              onClick={close}
-              className="text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 focus:ring-opacity-50"
-            >
-              <span className="sr-only">Close</span>
-              <XIcon aria-hidden-6 h-6" />
-            </button>
-          </div>
-          <div className="sm:flex sm:items-start">
-            <div className="mt-3 text-center sm:mt-0 max-w-prose sm:text-left">
-              <h3 className="text-3xl heading-1" id="modal-headline">
-                Please note...
-              </h3>
-              <div className="mt-2 prose-sm prose text-left text-gray-500">
-                <p>
-                  Whilst we endeavour to get your order out as quickly as
-                  possible, please be aware, we are a small business creating
-                  handmade items, and as such the processing time for orders{' '}
-                  <em>can</em> take up to 10 days.
-                </p>
-                <p>
-                  By checking the box below, you indicate that you are aware of
-                  this condition and agree to these terms.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="mt-5 space-y-4 sm:mt-4 sm:space-y-0 sm:space-x-4 sm:flex sm:justify-between">
-            <label
-              htmlFor="agree_to_terms"
-              className="flex items-center text-sm text-gray-900"
-            >
-              <input
-                id="agree_to_terms"
-                name="agree_to_terms"
-                type="checkbox"
-                checked={isChecked}
-                onChange={() => setIsChecked((prev) => !prev)}
-                className="w-4 h-4 text-pink-600 border-gray-300 rounded focus:ring-pink-500"
-              />
-              <span className="ml-2">Agree to terms</span>
-            </label>
-
-            <a
-              href={isChecked ? (checkout as string) : undefined}
-              className={`inline-flex items-center justify-center px-12 py-3 font-mono text-base font-medium text-center border border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 ${
-                isChecked
-                  ? 'text-pink-700 bg-pink-100 hover:bg-pink-200'
-                  : 'text-gray-700 bg-gray-100 cursor-not-allowed opacity-75'
-              }`}
-            >
-              Checkout
-            </a>
-          </div>
-        </DialogContent>
-      </div>
-    </DialogOverlay> */}
     </>
   );
 }
